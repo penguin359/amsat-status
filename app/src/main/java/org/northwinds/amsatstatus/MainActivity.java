b0b84d4463f81924ae6afa2103b833b475416ae9
Loren M. Lang <lorenl@north-winds.org>
2020-08-16 15:03:22 -0700

    Fixed hidden button at bottom and cleaned up warnings

--- app/src/main/java/org/northwinds/amsatstatus/MainActivity.java
+++ app/src/main/java/org/northwinds/amsatstatus/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+         */
     }
 
 }
