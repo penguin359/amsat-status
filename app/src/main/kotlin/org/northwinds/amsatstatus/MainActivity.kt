ab85d30e303d40995cd34aa643d62add30398027
Loren M. Lang <lorenl@north-winds.org>
2020-09-19 16:12:43 -0700

    Added a pop-up dialog asking for opt-in to analytics

--- app/src/main/kotlin/org/northwinds/amsatstatus/MainActivity.kt
+++ app/src/main/kotlin/org/northwinds/amsatstatus/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.northwinds.amsatstatus
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
@@ -7,10 +8,12 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
@@ -35,6 +38,30 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.title_analytics))
+
+        val animals = arrayOf(
+            getString(R.string.title_anonymous_usage),
+            getString(R.string.title_crash_reports))
+        var checkedItems = booleanArrayOf(false, false)
+        builder.setMultiChoiceItems(animals, checkedItems) { _, which, isChecked ->
+            checkedItems[which] = isChecked
+        }
+
+        builder.setPositiveButton("OK") { _, _ ->
+            prefs.edit {
+                putBoolean(getString(R.string.preference_enable_analytics), checkedItems[0])
+                putBoolean(getString(R.string.preference_enable_crash_reports), checkedItems[1])
+            }
+        }
+        //builder.setNegativeButton("Cancel", null)
+
+        val dialog = builder.create()
+        dialog.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
