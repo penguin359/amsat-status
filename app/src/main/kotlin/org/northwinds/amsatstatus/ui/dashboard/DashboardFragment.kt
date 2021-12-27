2499408802492885e7b0bb23b25a61bbd0126c2a
Loren M. Lang <lorenl@north-winds.org>
2021-12-26 23:52:06 -0800

    Changed dashboard to use an expandable list view
    It is currently hard-coded and incomplete otherwise.

--- app/src/main/kotlin/org/northwinds/amsatstatus/ui/dashboard/DashboardFragment.kt
+++ app/src/main/kotlin/org/northwinds/amsatstatus/ui/dashboard/DashboardFragment.kt
@@ -1,11 +1,11 @@
 package org.northwinds.amsatstatus.ui.dashboard
 
 import android.os.Bundle
+import android.util.ArrayMap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.Spinner
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -40,60 +40,84 @@ class DashboardFragment : Fragment() {
 
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+//        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val root = inflater.inflate(R.layout.fragment_dashboard_multi, container, false)
+        val listView = root.findViewById<ExpandableListView>(R.id.reports)
+        val reportGroups = arrayListOf(
+            mapOf("status" to "heard", "time" to "19:30"),
+            mapOf("status" to "not_heard", "time" to "19:45"),
+        )
+        val reportItems = arrayListOf(
+            arrayListOf(
+                mapOf("report" to "heard", "callsign" to "AB1C", "grid" to "CN85nl"),
+                mapOf("report" to "heard", "callsign" to "XY2Z", "grid" to "BP51fg"),
+            ),
+            arrayListOf(
+                mapOf("report" to "not_heard", "callsign" to "DL1FGH", "grid" to "DM43"),
+            ),
+        )
+        listView.setAdapter(SimpleExpandableListAdapter(requireActivity(),
+            reportGroups,
+            R.layout.fragment_dashboard_multi_group,
+            arrayOf("status", "time"),
+            intArrayOf(R.id.multi_status, R.id.multi_time),
+            reportItems,
+            R.layout.fragment_dashboard_multi_item,
+            arrayOf("report", "callsign", "grid"),
+            intArrayOf(R.id.multi_report, R.id.multi_callsign, R.id.multi_grid)))
         /*
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         */
-        val nameView: Spinner = root.findViewById(R.id.name)
-        nameView.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                dashboardViewModel.empty()
-            }
-
-            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, _id: Long) {
-                //val spinner = view as Spinner
-                //val id = spinner.selectedItemPosition
-                val id = position
-                val satellite_ids =
-                    resources.getStringArray(R.array.satellite_ids)
-                val params = Bundle().apply {
-                    val item = Bundle().apply {
-                        val satelliteNames =
-                            resources.getStringArray(R.array.satellite_names)
-                        putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "satellite")
-                        putString(FirebaseAnalytics.Param.ITEM_ID, satellite_ids[id])
-                        putString(FirebaseAnalytics.Param.ITEM_NAME, satelliteNames[id])
-                    }
-                    putParcelableArray(FirebaseAnalytics.Param.ITEMS, arrayOf(item))
-                    putString(FirebaseAnalytics.Param.ITEM_LIST_ID, "dashboard")
-                    putString(FirebaseAnalytics.Param.ITEM_LIST_NAME, "Dashboard")
-                }
-                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM, params)
-                /*
-                Toast.makeText(
-                    activity!!.applicationContext,
-                    "Sat Item " + position.toString() + " (" + satellite_ids[id] + ")",
-                    Toast.LENGTH_LONG
-                ).show()
-                */
-                dashboardViewModel.update(satellite_ids[id])
-            }
-        }
-        val reportView: RecyclerView = root.findViewById(R.id.reports)
-        dashboardViewModel.reports.observe(viewLifecycleOwner, Observer {
-            with(reportView) {
-                adapter = MyReportRecyclerViewAdapter(it)
-            }
-        })
-
-        val prefs = PreferenceManager(context).sharedPreferences
-        val satHeard = prefs.getString(requireContext().getString(R.string.preference_satellite), "")
-        val idx = requireContext().resources.getStringArray(R.array.satellite_ids).indexOf(satHeard)
-        if(idx >= 0)
-            nameView.setSelection(idx)
+//        val nameView: Spinner = root.findViewById(R.id.name)
+//        nameView.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                dashboardViewModel.empty()
+//            }
+//
+//            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, _id: Long) {
+//                //val spinner = view as Spinner
+//                //val id = spinner.selectedItemPosition
+//                val id = position
+//                val satellite_ids =
+//                    resources.getStringArray(R.array.satellite_ids)
+//        val params = Bundle().apply {
+//            val item = Bundle().apply {
+//                val satelliteNames =
+//                    resources.getStringArray(R.array.satellite_names)
+//                putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "satellite")
+//                putString(FirebaseAnalytics.Param.ITEM_ID, satellite_ids[id])
+//                putString(FirebaseAnalytics.Param.ITEM_NAME, satelliteNames[id])
+//            }
+//            putParcelableArray(FirebaseAnalytics.Param.ITEMS, arrayOf(item))
+//            putString(FirebaseAnalytics.Param.ITEM_LIST_ID, "dashboard")
+//            putString(FirebaseAnalytics.Param.ITEM_LIST_NAME, "Dashboard")
+//        }
+//        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM, params)
+//                /*
+//                Toast.makeText(
+//                    activity!!.applicationContext,
+//                    "Sat Item " + position.toString() + " (" + satellite_ids[id] + ")",
+//                    Toast.LENGTH_LONG
+//                ).show()
+//                */
+//                dashboardViewModel.update(satellite_ids[id])
+//            }
+//        }
+//        val reportView: RecyclerView = root.findViewById(R.id.reports)
+//        dashboardViewModel.reports.observe(viewLifecycleOwner, Observer {
+//            with(reportView) {
+//                adapter = MyReportRecyclerViewAdapter(it)
+//            }
+//        })
+//
+//        val prefs = PreferenceManager(context).sharedPreferences
+//        val satHeard = prefs.getString(requireContext().getString(R.string.preference_satellite), "")
+//        val idx = requireContext().resources.getStringArray(R.array.satellite_ids).indexOf(satHeard)
+//        if(idx >= 0)
+//            nameView.setSelection(idx)
 
         return root
     }
