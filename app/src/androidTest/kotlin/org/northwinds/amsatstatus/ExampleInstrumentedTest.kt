4175ef40c889437ead96214333620357c6ac37b2
Loren M. Lang <lorenl@north-winds.org>
2021-12-30 16:28:28 -0800

    Changed application id for development flavors
    Now both the production and development flavors can be installed at the same time.

--- app/src/androidTest/kotlin/org/northwinds/amsatstatus/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/org/northwinds/amsatstatus/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.northwinds.amsatstatus", appContext.packageName)
+        assertEquals("org.northwinds.amsatstatus.dev", appContext.packageName)
     }
 }
