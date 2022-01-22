e8652c33a57853b2308dc34f44f0e7a20941fcd8
Loren M. Lang <lorenl@north-winds.org>
2022-01-22 04:58:30 -0800

    Added in theme support from the upstream FragmentScenario
    With this change, the last remaining test is now fixed. The default
    theme that was being selected did not include the Content
    Description in the TimePicker sub-fields with some tests rely on.
    With this change, the HiltFragmentScenario behaves exacting like
    the upstream version which it was copied from except that it
    supports Hilt.
    
    All tests are now passing on this branch and it is now more
    reliable than main due to the use of Espresso idling resources.

--- app/src/debug/kotlin/org/northwinds/amsatstatus/testing/HiltTestActivity.kt
+++
@@ -1,7 +0,0 @@
-package org.northwinds.amsatstatus.testing
-
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class HiltTestActivity : AppCompatActivity()
