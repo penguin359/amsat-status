b2b453d0e3eabf524bdd35d03847608cb07e567c
Loren M. Lang <lorenl@north-winds.org>
2022-01-23 18:40:52 -0800

    Bumped all copyrights for files last modified in 2021

--- app/src/test/kotlin/org/northwinds/amsatstatus/AmsatApiTest.kt
+++ app/src/test/kotlin/org/northwinds/amsatstatus/AmsatApiTest.kt
@@ -1,5 +1,5 @@
 /**********************************************************************************
- * Copyright (c) 2020 Loren M. Lang                                               *
+ * Copyright (c) 2021 Loren M. Lang                                               *
  *                                                                                *
  * Permission is hereby granted, free of charge, to any person obtaining a copy   *
  * of this software and associated documentation files (the "Software"), to deal  *
